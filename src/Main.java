ea83a87426cdbcea87a20bfd096112ce4f794364
Cristiann-Paredes <paredes30@hotmail.es>
2023-09-01 14:19:32 -0500

    ini

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,8 @@ public class Main {
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
+
+
         // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
 
